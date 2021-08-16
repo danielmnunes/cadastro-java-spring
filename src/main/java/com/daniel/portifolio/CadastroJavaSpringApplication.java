11fd26aebfabcab1570dd002bd12301c502b08ea
Daniel Nunes <danielmnunes08@gmail.com>
2021-08-16 09:44:10 -0300

    create project

---
+++ src/main/java/com/daniel/portifolio/CadastroJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.portifolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/portifolio/CadastroJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.portifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
